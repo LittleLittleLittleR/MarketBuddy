638999dfac44871c688432e872e5264ae37df08c
RaphaelYeo <Raphael_YEO@htx.gov.sg>
2026-05-18 00:13:24 +0800

    instantiate fastapi endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,46 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"root": "welcome to MarketBuddy"}
+
+# AUTH
+@app.post("/signup/")
+def signup():
+    # TODO
+    return None
+
+@app.post("/login/")
+def login():
+    # TODO
+    return None
+
+@app.post("/logout/")
+def logout():
+    # TODO
+    return None
+
+# STOCKS
+@app.get("/stocks")
+def get_all_stocks(limit: int = 10, offset: int = 0):
+    # TODO
+    return None
+
+@app.get("/stocks/{stock_id}")
+def get_stock(stock_id: int):
+    # TODO
+    return None
+
+# WATCHLIST
+@app.get("/watchlist/{user_id}")
+def get_watchlist(user_id: int):
+    # TODO
+    return None
+
+@app.post("/watchlist/{user_id}")
+def add_watchlist(user_id: int):
+    # TODO
+    return None
+
+@app.delete("/watchlist/{user_id}")
+def remove_watchlist(user_id: int):
+    # TODO
+    return None
