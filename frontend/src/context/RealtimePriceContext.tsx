d122e09e667108135d781c7302ed9478ba531843
Rayyan <rwrayyan2004@gmail.com>
2026-06-28 21:57:46 +0800

    changed ws url

--- frontend/src/context/RealtimePriceContext.tsx
+++ frontend/src/context/RealtimePriceContext.tsx
@@ -64,8 +64,8 @@ export const RealtimePriceProvider = ({ children }: { children: React.ReactNode 
       return;
     }
 
-    // const WS_URL = `wss://orbital-fastapi-backend-production.up.railway.app/ws/prices?token=${session.access_token}`
-    const WS_URL = `ws://localhost:8000/ws/prices?token=${session.access_token}`
+    const WS_URL = `wss://orbital-fastapi-backend-production.up.railway.app/ws/prices?token=${session.access_token}`
+    // const WS_URL = `ws://localhost:8000/ws/prices?token=${session.access_token}`
     const rws = new ReconnectingWebSocket(WS_URL, [], {
       maxReconnectionDelay: 20000, // Caps exponential backoff at 10 seconds max
       minReconnectionDelay: 5000,  // Starts retrying after 5 second if connection drops
